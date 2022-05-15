912d008dc0290a825c18c9aea80d0f296280e2b6
Quin Eddy <qkeddy@gmail.com>
2022-05-14 22:11:15 -0400

    Setup initial file scaffolding

---
+++ index.js
@@ -0,0 +1,15 @@
+// Import React JavaScript Library
+import React from "react";
+
+// Import React DOM-specific methods
+import ReactDOM from "react-dom";
+
+// TODO Document
+import App from "./App";
+
+// Import minimized Bootstrap CSS
+import "bootstrap/dist/css/bootstrap.min.css";
+
+// ReactDOM.render takes a React component, or tree of React components and (eventually) renders them to the DOM.
+// The first argument is the component we want to render (<App/>), and second is the target element to render to (#root)
+ReactDOM.render(<App />, document.getElementById("root"));
