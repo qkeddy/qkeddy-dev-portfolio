2bd4f59884cef0790329fed94ef342e58f94f0f0
Quin Eddy <qkeddy@gmail.com>
2022-05-17 19:39:02 -0400

    Setup navbar with responsive design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,39 +4,35 @@ import React from "react";
 export default function Nav({ currentPage, handlePageChange }) {
     return (
         // Referencing Bootstrap style classes
-        <ul className="nav nav-tabs">
-            <li>
-                <a href="#about" onClick={() => handlePageChange("About")} className={
-                    currentPage === "About"
-                        ? "nav-link active"
-                        : "nav-link"}>
-                    About
+        <nav class="navbar navbar-expand-lg bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    {/* <img src="../../public/logo192.png" alt="" width="30" height="24" class="d-inline-block align-text-top"> */}
+                    <h1>Quinlan Eddy</h1>
                 </a>
-            </li>
-            <li>
-                <a href="#contact" onClick={() => handlePageChange("Contact")} className={
-                    currentPage === "Contact"
-                        ? "nav-link active"
-                        : "nav-link"}>
-                    Contact
-                </a>
-            </li>
-            <li>
-                <a href="#portfolio" onClick={() => handlePageChange("Portfolio")} className={
-                    currentPage === "Portfolio"
-                        ? "nav-link active"
-                        : "nav-link"}>
-                    Portfolio
-                </a>
-            </li>
-            <li>
-                <a href="#resume" onClick={() => handlePageChange("Resume")} className={
-                    currentPage === "Resume"
-                        ? "nav-link active"
-                        : "nav-link"}>
-                    Resume
-                </a>
-            </li>
-        </ul>
+                <ul className="nav nav-pills flex-column flex-sm-row justify-content-end">
+                    <li>
+                        <a href="#about" onClick={() => handlePageChange("About")} className={currentPage === "About" ? "nav-link active" : "nav-link"}>
+                            About
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#contact" onClick={() => handlePageChange("Contact")} className={currentPage === "Contact" ? "nav-link active" : "nav-link"}>
+                            Contact
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#portfolio" onClick={() => handlePageChange("Portfolio")} className={currentPage === "Portfolio" ? "nav-link active" : "nav-link"}>
+                            Portfolio
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#resume" onClick={() => handlePageChange("Resume")} className={currentPage === "Resume" ? "nav-link active" : "nav-link"}>
+                            Resume
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     );
 }
