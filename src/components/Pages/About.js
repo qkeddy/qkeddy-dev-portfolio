a1dc87be79d97713876dc74cb5dab2daf17d4ea5
Quin Eddy <qkeddy@gmail.com>
2022-05-18 01:16:29 -0400

    Added about page with picture

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from "react";
 
 export default function About() {
     return (
-        <div>
-            <p>
-                Strategic and tactical problem-solver with expertise in technical design, deployment, and maintenance of enterprise software. Committed and focused communicator with demonstrated success in building and introducing new technologies
-                while conceptualizing improvements for interoperability of various systems. Thrive in entrepreneurial environments where meeting deadlines, creativity, and teamwork are essential. A graduate from UPenn Full-stack Coding Bootcamp with
-                engineering skills in JavaScript, CSS, React.js, responsive web design, and the full MERN stack.
-            </p>
+        <div className="container  pb-4" style={{ lineHeight: 2 }}>
+            <div className="row">
+                <div className="col-lg-7 pt-4 pt-lg-0">
+                    <img src="https://avatars.githubusercontent.com/u/9737814" class="img-thumbnail" alt="Quinlan Eddy profile picture" width="150"></img>
+                    <p>
+                        Strategic and tactical problem-solver with expertise in technical design, deployment, and maintenance of enterprise software. Committed and focused communicator with demonstrated success in building and introducing new
+                        technologies while conceptualizing improvements for interoperability of various systems. Thrive in entrepreneurial environments where meeting deadlines, creativity, and teamwork are essential. A graduate from UPenn Full-stack
+                        Coding Bootcamp with engineering skills in JavaScript, CSS, React.js, responsive web design, and the full MERN stack.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 }
