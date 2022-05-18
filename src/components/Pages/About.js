9d51aefc70f24527aa891875be693e54be612795
Quin Eddy <qkeddy@gmail.com>
2022-05-18 00:41:59 -0400

    Removed unnecessary functionality in helpers. Added About.

--- src/components/Pages/About.js
+++ src/components/Pages/About.js
@@ -3,8 +3,11 @@ import React, { useState, useEffect } from "react";
 export default function About() {
     return (
         <div>
-            <h1>About Page</h1>
-            <p>Add details</p>
+            <p>
+                Strategic and tactical problem-solver with expertise in technical design, deployment, and maintenance of enterprise software. Committed and focused communicator with demonstrated success in building and introducing new technologies
+                while conceptualizing improvements for interoperability of various systems. Thrive in entrepreneurial environments where meeting deadlines, creativity, and teamwork are essential. A graduate from UPenn Full-stack Coding Bootcamp with
+                engineering skills in JavaScript, CSS, React.js, responsive web design, and the full MERN stack.
+            </p>
         </div>
     );
 }
